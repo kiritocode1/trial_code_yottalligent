d9933c1a220444b74573040db5772f59835f7c4c
kiritocode1 <kathawalearyan9@gmail.com>
2022-09-17 18:06:11 +0530

    Initial COMMIT

--- functions.php
+++ functions.php
@@ -3,42 +3,38 @@
 function check_login($con)
 {
 
-	if(isset($_SESSION['user_id']))
-	{
+	if (isset($_SESSION['user_id'])) {
 
 		$id = $_SESSION['user_id'];
-		$query = "select * from users where user_id = '$id' limit 1";
+		$query = "select user_name from users ";
 
-		$result = mysqli_query($con,$query);
-		if($result && mysqli_num_rows($result) > 0)
-		{
+		$result = mysqli_query($con, $query);
+		if ($result && mysqli_num_rows($result) > 0) {
 
-			$user_data = mysqli_fetch_assoc($result);
+			$user_data = mysqli_fetch_all($result);
 			return $user_data;
 		}
 	}
 
-	//redirect to login 
+	//redirect to login
 	header("Location: login.php");
 	die;
-
 }
 
 function random_num($length)
 {
 
 	$text = "";
-	if($length < 5)
-	{
+	if ($length < 5) {
 		$length = 5;
 	}
 
-	$len = rand(4,$length);
+	$len = rand(4, $length);
 
-	for ($i=0; $i < $len; $i++) { 
+	for ($i = 0; $i < $len; $i++) {
 		# code...
 
-		$text .= rand(0,9);
+		$text .= rand(0, 9);
 	}
 
 	return $text;
--- index.php
+++ index.php
@@ -1,23 +1,31 @@
-<?php 
+<?php
 session_start();
 
-	include("connection.php");
-	include("functions.php");
-	
-	$user_data = check_login($con);
+include("connection.php");
+include("functions.php");
+
+$user_data = check_login($con);
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>YottaIntelligent Pvt Limited </title>
 </head>
+
 <body>
 
 	<a href="logout.php">Logout</a>
 	<h1>This is the index page</h1>
 	<br>
-	Hello, <?php echo $user_data['user_name']; ?>
+	Hello, <?php
+			foreach ($user_data as $user) {
+				print_r($user);
+				echo "<br>";
+			}
+			?>
 </body>
+
 </html>
